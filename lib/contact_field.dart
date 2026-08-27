f4624814bef5d5febb651d79500ad3f7158aa680
Zeyad Samy <zeyadsamy-flutter@users.noreply.github.com>
2026-08-28 02:39:20 +0300

    Clean up widget tree by removing unnecessary Padding

--- lib/contact_field.dart
+++ lib/contact_field.dart
@@ -13,16 +13,14 @@ class ContactField extends StatelessWidget {
         borderRadius: BorderRadius.all(Radius.circular(10)),
         color: Colors.white,
       ),
+      padding: const EdgeInsets.all(15.0),
       margin: const EdgeInsets.symmetric(horizontal: 25),
-      child: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Row(
-          children: [
-            Icon(iconField, size: 30, color: const Color(0xff2a465e)),
-            const SizedBox(width: 20),
-            Text(text, style: const TextStyle(color: Colors.black)),
-          ],
-        ),
+      child: Row(
+        children: [
+          Icon(iconField, size: 30, color: const Color(0xff2a465e)),
+          const SizedBox(width: 20),
+          Text(text, style: const TextStyle(color: Colors.black)),
+        ],
       ),
     );
   }
