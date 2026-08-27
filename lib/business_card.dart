4a8fd12d4482bfda442ec5c1c291c959f4b5d62b
Zeyad Samy <zeyadsamy-flutter@users.noreply.github.com>
2026-08-28 01:03:43 +0300

    Clean up widget tree by removing unnecessary Center

--- lib/business_card.dart
+++ lib/business_card.dart
@@ -6,31 +6,29 @@ class BusinessCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Color(0xff2a465e),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage("assets/images/zeyad.png"),
-              radius: 100,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage("assets/images/zeyad.png"),
+            radius: 100,
+          ),
+          SizedBox(height: 10),
+          Text(
+            "Zeyad Samy",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+              fontStyle: FontStyle.italic,
             ),
-            SizedBox(height: 10),
-            Text(
-              "Zeyad Samy",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 25,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-            SizedBox(height: 10),
-            Text("FLUTTER DEVELOPER", style: TextStyle(color: Colors.white)),
-            SizedBox(height: 10),
-            ContactField(text: "01270249382", iconField: Icons.phone),
-            SizedBox(height: 10),
-            ContactField(text: "zeyaddsamy87@gmail.com", iconField: Icons.mail),
-          ],
-        ),
+          ),
+          SizedBox(height: 10),
+          Text("FLUTTER DEVELOPER", style: TextStyle(color: Colors.white)),
+          SizedBox(height: 10),
+          ContactField(text: "01270249382", iconField: Icons.phone),
+          SizedBox(height: 10),
+          ContactField(text: "zeyaddsamy87@gmail.com", iconField: Icons.mail),
+        ],
       ),
     );
   }
